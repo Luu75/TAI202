db4c59ad1f8c5647f4d34edeba11e3866301a61a
Luis <123045772@upq.edu.mx>
2026-02-10 20:20:07 -0600

    endpoints
    Se agrego endpoints para checar la funcionalidad de los mismos

--- miAPI/main.py
+++ miAPI/main.py
@@ -16,3 +16,20 @@ async def bienvenido():
         "estatus": "activo"
     }
 
+# Endpoint con parámetro obligatorio
+@app.get("/usuarios/{id}")
+async def obtener_usuario(id: int):
+    return {
+        "mensaje": "Usuario encontrado",
+        "id_usuario": id
+    }
+
+# Endpoint con parámetro opcional
+@app.get("/saludo")
+async def saludo(nombre: str = "Invitado"):
+    return {
+        "mensaje": f"Hola {nombre}"
+    }
+
+
+
